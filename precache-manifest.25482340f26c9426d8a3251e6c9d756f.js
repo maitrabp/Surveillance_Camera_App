8a35d327037cfdfc838edc93ebe7341bd2e96821
maitrapatel <maitrabp@umich.edu>
2020-11-01 13:23:27 -0500

    Updates

---
+++ precache-manifest.25482340f26c9426d8a3251e6c9d756f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "233e89d5546d28ae6df3f1458c7f7da0",
+    "url": "/Surveillance_Camera_App/index.html"
+  },
+  {
+    "revision": "0bd91614b5a8ccae3022",
+    "url": "/Surveillance_Camera_App/static/css/main.c3791041.chunk.css"
+  },
+  {
+    "revision": "a2d3cf51bff12774afd3",
+    "url": "/Surveillance_Camera_App/static/js/2.a34b0507.chunk.js"
+  },
+  {
+    "revision": "0192aefbe120e03c8b4e34d96152e68c",
+    "url": "/Surveillance_Camera_App/static/js/2.a34b0507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bd91614b5a8ccae3022",
+    "url": "/Surveillance_Camera_App/static/js/main.68e0bc31.chunk.js"
+  },
+  {
+    "revision": "18f1222f8d9e4899f612",
+    "url": "/Surveillance_Camera_App/static/js/runtime-main.2e3252a3.js"
+  }
+]);
